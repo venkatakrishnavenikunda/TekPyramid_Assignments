9232d144c2745b3ac2544d3e7852d9c1e2e541e0
venkatakrishnavenikunda <117799350+venkatakrishnavenikunda@users.noreply.github.com>
2025-11-04 17:28:25 +0530

    msc

--- src/Assignment_2/PunctuationCount.java
+++ src/Assignment_2/PunctuationCount.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PunctuationCount {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a string:");
+        System.out.println("Enter the string:");
         String s=sc.nextLine();
         System.out.println(s);
         char[] str=s.toCharArray();
