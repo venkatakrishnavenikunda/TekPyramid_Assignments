0e1c1b385ac2292c063da887d55c6c62263ee53b
venkatakrishnavenikunda <117799350+venkatakrishnavenikunda@users.noreply.github.com>
2025-11-04 19:56:28 +0530

    msc

--- src/Assignment_2/SortUsingBubble.java
+++ src/Assignment_2/SortUsingBubble.java
@@ -1,4 +1,40 @@
 package Assignment_2;
 
+import java.util.Arrays;
+
+//public class SortUsingBubbles{
+//    public static void main(String[] args){
+//        int[]a={16,9,5,4,7,12,3,20,15};
+//        System.out.println("Before sorting:"+Arrays.toString(a));
+//        for(int i=0; i<a.length;i++){
+//            for(int j=0;j<a.length-1;j++){
+//                if(a[j]>a[j+1]){
+//                    int temp=a[j];
+//                    a[j]=a[j+1];
+//                    a[j+1]=temp;
+//                }
+//            }
+//        }
+//        System.out.println("After sorting:"+Arrays.toString(a));
+//    }
+//}
+
+
+//Using one for loop
 public class SortUsingBubble {
+    public static void main(String[] args){
+        int[]a={16,9,5,4,7,12,3,20,15};
+        System.out.println(Arrays.toString(a));
+        for(int i=0;i<a.length-1;i++){
+            if(a[i]>a[i+1]){
+                int temp=a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+                i=-1;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
 }
+
+
