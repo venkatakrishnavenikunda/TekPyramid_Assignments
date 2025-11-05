47a0405ded0653739fc852bc096c8ab135e7d4f7
venkatakrishnavenikunda <117799350+venkatakrishnavenikunda@users.noreply.github.com>
2025-11-05 21:40:33 +0530

    21:40

--- src/Assignment_3/DivideStringIntoNParts.java
+++ src/Assignment_3/DivideStringIntoNParts.java
@@ -1,6 +1,6 @@
 package Assignment_3;
 
-//To divide the string equal N parts
+//1.To divide the string equal N parts
 
 //public class DivideStringIntoNParts {
 //    public static void main(String[] args) {
@@ -24,20 +24,55 @@ package Assignment_3;
 
 
 
-//To divide the string into N parts
-public class DivideStringIntoNParts{
+//2.To divide the string into N parts
+//public class DivideStringIntoNParts{
+//    public static void main(String[] args){
+//        String s="HELLOWORL";
+//        int n=3;
+//        int len=s.length();
+//        int size=len/n;
+//        int start=0, end=size;
+//        for(int i=0; i<size;i++){
+//            System.out.println(s.substring(start,end));
+//            start+=size;
+//            end+=size;
+//        }
+//        if(start<len) {
+//            System.out.println("Extra characters after dividing " + n + " parts");
+//            System.out.println(s.substring(start));
+//        }
+//    }
+//}
+
+
+
+
+
+//3. To divide string into N parts  using if condition if string length is not divided with n otherwise use else condition
+public class DivideStringIntoNParts {
     public static void main(String[] args){
         String s="HELLOWORLD";
         int n=3;
         int len=s.length();
         int size=len/n;
-        int start=0, end=size;
-        for(int i=0; i<size;i++){
-            System.out.println(s.substring(start,end));
-            start+=size;
-            end+=size;
+        //System.out.println(size);
+        if(len%n!=0){
+            System.out.println("Using if condition");
+            int i;
+            for(i=0;i+size<len;i+=size){
+                System.out.println(s.substring(i,i+size));
+            }
+            if(i<len) {
+                System.out.println("Extra characters after dividing "+n+" parts");
+                System.out.println(s.substring(i));
+            }
         }
-        System.out.println(s.substring(start));
-
+        else{
+            System.out.println("Using else condition");
+            for(int i=0;i<len;i+=size){
+                System.out.println(s.substring(i,i+size));
+            }
+            System.out.println();
+        }
     }
 }
